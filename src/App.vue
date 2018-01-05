9d9a8d277bea9d20e495924934f814d0e948b581
Oganesyan Artur <arturris@mail.ru>
2018-01-05 16:05:12 -0600

    Commit #2

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div>
     <h1>{{title}}</h1>
     <ninjas></ninjas>
@@ -24,4 +24,48 @@ export default {
   h1{
     color: purple;
   }
+</style>  -->
+
+<template>
+  <div>
+    <app-header v-bind:title="title" v-on:changeTitle="updateTitle($event)"></app-header>
+    <app-ninjas v-bind:ninjas="ninjas"></app-ninjas>
+    <app-footer v-bind:title="title"></app-footer>
+  </div>
+</template>
+
+<script>
+ import Header from './components/Header.vue'
+ import Footer from './components/Footer.vue'
+ import Ninjas from './components/Ninjas.vue'
+
+export default {
+  components: {
+    'app-header': Header,
+    'app-footer': Footer,
+    'app-ninjas': Ninjas,
+  },
+  data () {
+    return {
+      ninjas: [
+        {name: 'Artur', speciality: 'Vue Component', show: false},
+        {name: 'Ryu', speciality: 'Html', show: false},
+        {name: 'Hitoshi', speciality: 'CSS', show: false},
+        {name: 'Tango', speciality: 'JS', show: false},
+        {name: 'Kami', speciality: 'Webpack', show: false},
+        {name: 'Yoshi', speciality: 'PHP', show: false},
+      ],
+      title: "Vue Ninjas",
+    }
+  },
+  methods: {
+    updateTitle: function(updatedTitle) {
+      this.title = updatedTitle
+    }
+  }
+}
+</script>
+
+<style >
+
 </style>
